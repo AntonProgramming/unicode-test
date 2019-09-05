1506fe00b77fcb20e9cfd057c3b925ee1f05d0b5
AntonProgramming <anton.popov1983@mail.ru>
2019-09-06 01:08:40 +0300

    06.09.19

--- newfile.py
+++ newfile.py
@@ -2,6 +2,6 @@ def unicode_test (value):
 	import unicodedata
 	name=unicodedata.name (value)
 	print('value="%s", name="%s" ' %(value, name))
-	#print(decoding)
+	print(name, encoding ='utf-8')
 
 unicode_test(input('Input a sign:'))
