cf337d8e0d71fdc0b27781043316b9056ef8a038
AntonProgramming <anton.popov1983@mail.ru>
2019-09-22 12:49:12 +0300

    22 sept

--- newfile.py
+++ newfile.py
@@ -2,6 +2,15 @@ def unicode_test (value):
 	import unicodedata
 	name=unicodedata.name (value)
 	print('value="%s", name="%s" ' %(value, name))
-	print(name, encoding ='utf-8')
+	print(name.encode ('utf-8'))
+	print(name.encode('iso-8859-1'))
+	print(name.encode('windows-1252'))
+	#print(name.encode('unicode'))
+	print(name.encode('utf-16'))
+	a=name.encode('utf-16')
+	print(type(a))
+	#print(name.encode('ucs-2'))
+	print(name.encode('ascii'))
+#	print(name.decode('Unicode'))
 
-unicode_test(input('Input a sign:'))
+unicode_test (input('Input a sign:'))
--- unicode-test.py
+++ unicode-test.py
@@ -4,6 +4,11 @@ def unicode_test (value):
 	value2=unicodedata.lookup(name)
 	print('value="%s", name="%s",value2="%s" ' %(value, name, value2))
 
-
 unicode_test('\u00e9')
 print(len('\u00e9'))
+
+value='\u00e9'
+print (value.encode('utf-8'))
+print(value.encode('ascii', 'ignore'))
+print (value)
+unicode_test(input( ))
